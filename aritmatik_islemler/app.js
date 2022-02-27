6b1841ccb7f3d1fb10bad66cc6916b8478bb888f
iferhatzdemir <ferhatzdemir9@gmail.com>
2022-02-27 15:24:31 +0300

    çift mi tek mi bulan kod eklendi

---
+++ aritmatik_islemler/app.js
@@ -0,0 +1,66 @@
+/*
+//number veri türünü tanımlamak
+let price=100;
+let tax=0.18;
+let priceTax=price*tax;
+let totalPrice=price+priceTax;
+console.log(totalPrice);
+console.log("-----------------------");
+//artırma ve azaltma işlemleri 
+let counter=320;
+counter=counter+1;
+console.log(counter);
+console.log("-----------------------");
+let counterTwo=0;
+counter=counter+1; // uzun yontem
+counterTwo+=1;//artıma işlemi
+console.log(counterTwo);
+
+for(let x=0;x<20;x++){
+    counterTwo+=1;
+    console.log(counterTwo)
+    console.log("|------------------------------------|");
+}
+
+
+console.log("/////////////////")
+let sayac=20;
+
+sayac--;
+console.log(sayac);
+
+for(let y=0; y<sayac; y++){
+    sayac-=1;
+    sayac%=2;
+    console.log(sayac);
+    y*=2;
+    
+
+}
+let cos=12;
+cos/=3;
+console.log(cos);
+
+let mod=403;
+mod%=2;
+console.log(mod);
+//işlem önceliği ;
+*/
+
+
+var ciftArray=[];
+var tekArray=[];
+var mainArray=[23,3242,34,324,2,3,4,4,4,5,2,3,4,24,87];
+for(let a=0; a<mainArray.length;a++){
+if(mainArray[a]%2==0){
+ let doubleEleman= mainArray[a];
+ ciftArray.push(doubleEleman);
+}else{
+    let SingleEleman=mainArray[a];
+    tekArray.push(SingleEleman);
+}
+}
+
+console.log(tekArray);
+console.log("----------------")
+console.log(ciftArray);
