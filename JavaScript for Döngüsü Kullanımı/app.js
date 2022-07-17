393a5170f80d3ae7b93a93efb0859d67a9da384b
iferhatzdemir <ferhatzdemir9@gmail.com>
2022-07-17 16:11:43 +0300

    for konusu tamamlanıldı

--- JavaScript for Döngüsü Kullanımı/app.js
+++ JavaScript for Döngüsü Kullanımı/app.js
@@ -1,8 +1,34 @@
 let users = ["ahmet", "mehmet", "ramazan", "mustafa"];
-let listDom = document.querySelector("userList");
+let userListDOM = document.querySelector("#userList");
 for (var i = 0; i < users.length; i++) {
-  let liDom = document.createElement("li");
-  console.log(users[i]);
-  liDom.innerHTML = users[i];
-  liDom.append();
+  const liDOM = document.createElement("li");
+  liDOM.innerHTML = users[i];
+  userListDOM.appendChild(liDOM);
 }
+
+// For Dongusu
+// https://developer.mozilla.org/tr/docs/Web/JavaScript/Guide/Loops_and_iteration
+
+// for ([başlangıçAtaması]; [koşulİfadesi]; [arttırımİfadesi]) {
+// yapilacak islem
+// }
+
+// let users = ["Lorem", "Ipsum", "Dolor"];
+
+// // for (let index = 0; index < 10; index++) {
+// //   console.log(index)
+// // }
+
+// // let index = 0
+
+// // for (; index < 10; index++) {
+// //   console.log(index)
+// // }
+
+// const userListDOM = document.querySelector("#userList");
+
+// for (index = 0; index < users.length; index++) {
+//   const liDOM = document.createElement("li");
+//   liDOM.innerHTML = users[index];
+//   userListDOM.appendChild(liDOM);
+// }
--- Liste_icerisine_ulasmak_yenioge_ekleme/app.js
+++ Liste_icerisine_ulasmak_yenioge_ekleme/app.js
@@ -1,20 +1,17 @@
 // liste içerisindenki son elemana ulasmak veya eleman eklemek
 
-let Firstitem=document.querySelector("ul#list>li:first-child")
-Firstitem.innerHTML="ilk öge"
+let Firstitem = document.querySelector("ul#list>li:first-child");
+Firstitem.innerHTML = "ilk öge";
 console.log(Firstitem);
 
+let lastitem = document.querySelector("ul#list>li:last-child");
+lastitem.innerHTML = "son öge";
+console.log(lastitem);
 
+let ulDom = document.querySelector("ul#list");
+let liDom = document.createElement("li");
+liDom.innerHTML = "ensona eklemek";
+ulDom.append(liDom);
 
-let lastitem=document.querySelector("ul#list>li:last-child")
-lastitem.innerHTML="son öge"
-console.log(lastitem)
-
-
-let ulDom=document.querySelector("ul#list")
-let liDom=document.createElement("li");
-liDom.innerHTML="ensona eklemek";
-ulDom.append(liDom)
-
-liDom.innerHTML="ilk başa eklemk";
+liDom.innerHTML = "ilk başa eklemk";
 ulDom.prepend(liDom);
