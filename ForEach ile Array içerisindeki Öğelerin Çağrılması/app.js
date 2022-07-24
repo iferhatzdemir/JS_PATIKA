f0678167dd7014b445dcd30d23fd32eef8c202b1
iferhatzdemir <ferhatzdemir9@gmail.com>
2022-07-24 13:15:33 +0300

    forEach konusunda konu anlatımı dinlenildi

---
+++ ForEach ile Array içerisindeki Öğelerin Çağrılması/app.js
@@ -0,0 +1,26 @@
+const PRODUCTS = [
+  "Laptop",
+  "Phone",
+  "Speaker",
+  "Desktop PC",
+  "Server",
+  "Mouse",
+  "Keyboard",
+];
+
+// PRODUCTS.forEach((product, index, array) => console.log(PRODUCTS));
+
+PRODUCTS.forEach(
+  (product, index, array) => (array[index] = `${product.toUpperCase()}`)
+); // toUpperCase
+
+console.log(PRODUCTS);
+
+const userListDOM = document.querySelector("#userList");
+
+PRODUCTS.forEach((product, index, array) => {
+  const liDOM = document.createElement("li");
+  liDOM.innerHTML = product;
+  console.log(product[index]);
+  userListDOM.appendChild(liDOM);
+});
